800987e7aaac27c5afe4fad5c92bfb5ba17f8ee0
GT458 <isohrob1@gmail.com>
2021-07-02 17:47:28 -0400

    updated landing page

--- frontend/fitbook.jsx
+++ frontend/fitbook.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thanos from '../app/assets/images/thanos.gif'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-
-  ReactDOM.render(<h1>Welcome to Fitbook!</h1>, root);
+  const tempEle = () => {
+    return (
+      <div>
+        <h1>Welcome to Fitbook</h1>
+        <img src={thanos} alt="thanos"></img>
+      </div>
+    )
+  }
+  ReactDOM.render(tempEle, root);
 })
