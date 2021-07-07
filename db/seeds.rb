bdb24e6c34f01e259d05a22ebd15b37fdc1f8561
GT458 <isohrob1@gmail.com>
2021-07-07 10:55:39 -0400

    fixed seed file error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'demo@demo.com' password: 'password', fname: 'Demo', lname: 'User', birthday:'01-01-1999', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
-User.create(email: 'optimus@gmail.com' password: 'password', fname: 'Optimus', lname: 'Prime', birthday:'03-01-1986', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
-User.create(email: 'captaina@gmail.com' password: 'password', fname: 'Steve', lname: 'Rogers', birthday:'07-04-1918', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
-User.create(email: 'sohrob@gmail.com' password: 'password', fname: 'Sohrob', lname: 'Ibrahimi', birthday:'04-08-1999', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
+User.create(email: 'demo1@demo.com', password: 'password', fname: 'Demo', lname: 'User', birthday:'01-01-1999', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
+User.create(email: 'optimus1@gmail.com', password: 'password', fname: 'Optimus', lname: 'Prime', birthday:'03-01-1986', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
+User.create(email: 'captaina1@gmail.com', password: 'password', fname: 'Steve', lname: 'Rogers', birthday:'07-04-1918', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
+User.create(email: 'sohrob@gmail1.com', password: 'password', fname: 'Sohrob', lname: 'Ibrahimi', birthday:'04-08-1999', gender: 'Male', profile_photo_url: '', cover_photo_url: '', school: '', bio: '', work: '', city: '')
