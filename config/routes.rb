02242ff9a52786e80b12ca9aa6d0cdc547f94a74
GT458 <isohrob1@gmail.com>
2021-07-13 11:50:54 -0400

    added posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :users,  only: [:create, :index, :show, :update]
+    resources :posts, only: [:create, :index, :show, :update, :delete]
     resource :session, only: [:create, :destroy]
   end
 
--- frontend/components/navigation/nav_bar.jsx
+++ frontend/components/navigation/nav_bar.jsx
@@ -22,7 +22,12 @@ class NavBar extends React.Component {
         </div>
         <div className='right'>
           <div className='nav-buttons-container'>
-            <button className='nav-btn profile-btn'>P</button>
+            <div className='profile-btn'>
+              <div className='thumbnail'>
+                <img src={'https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-9/78117715_3302453119826931_6725932744179712000_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=67GSNo2B_IEAX_LuFnV&_nc_oc=AQkj2xKG2b0cJ4RkfERvJ-kt_JMAJrqP790jFTO8r0mNY681bo9g3arXIBY9jxFY2a4&_nc_ht=scontent-iad3-1.xx&oh=38393f8d80a6cc51ed2cd22cf1a65e9c&oe=60F03844'}></img>
+              </div>
+              <span>{this.props.currentUser.fname[0].toUpperCase() + this.props.currentUser.fname.slice(1)}</span>
+              </div>
             <button className='nav-btn menu'>M</button>
             <button className='nav-btn notif-btn'>N</button>
             <button className='nav-btn dropdown-btn'>D</button>
