dc6491f4919ede3f729cdd7db4228c682a2de501
GT458 <isohrob1@gmail.com>
2021-09-30 16:56:28 -0400

    styling navbar

--- frontend/components/navigation/nav_bar.jsx
+++ frontend/components/navigation/nav_bar.jsx
@@ -19,6 +19,9 @@ class NavBar extends React.Component {
         <div className='middle'>
           <div className='buttons-container'>
             {/* <a href='https://github.com/GT458/fitbook'><img src='https://pngimg.com/uploads/github/github_PNG40.png'></img></a> */}
+            <a href='https://github.com/GT458/fitbook'><button className='nav-btn github'></button> </a>
+            <a href='https://github.com/GT458/fitbook'><button className='nav-btn linkedin'></button> </a>
+            
           </div>
         </div>
         <div className='right'>
@@ -31,10 +34,9 @@ class NavBar extends React.Component {
               <span>{this.props.currentUser.fname[0].toUpperCase() + this.props.currentUser.fname.slice(1)}</span>
               </div>
                 </Link>
-            <a href='https://github.com/GT458/fitbook'><button className='nav-btn menu'></button> </a>
-            <a href='https://github.com/GT458/fitbook'><button className='nav-btn menu'></button> </a>
+            <button className='nav-btn .notif-btn'>N</button> 
             {/* <button className='nav-btn notif-btn'></button> */}
-            <button className='nav-btn dropdown-btn' onClick={() => this.props.logout()}></button>
+            <button className='nav-btn logout-btn' onClick={() => this.props.logout()}></button>
 
           </div>
         </div>
