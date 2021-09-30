35daa9b000206a796cb52a9ff4b8874297efd3ff
GT458 <isohrob1@gmail.com>
2021-09-30 18:29:15 -0400

    add remove friend option

--- frontend/components/profile/profile_components/profile_header.jsx
+++ frontend/components/profile/profile_components/profile_header.jsx
@@ -25,7 +25,8 @@ class ProfileHeader extends React.Component {
       if (currentUserFriends.length >= 1) {
         for (let i = 0; i < currentUserFriends.length; i++) {
           if (currentUserFriends[i].user_id2 === user.id || currentUserFriends[i].user_id1 === user.id) {
-            return <button className='remove-fr-btn' onClick={() => this.props.deleteFriend(currentUserFriends[i].id)}></button>
+            // debugger;
+            return <button className='remove-fr-btn' onClick={() => this.props.deleteFriend(currentUserFriends[i].id)}>Remove Friend</button>
           }
         }
       }
