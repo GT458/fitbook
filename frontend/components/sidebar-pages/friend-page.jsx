c581c9d8da4a2dcbc145c9c9b8aa61efa292e5db
GT458 <isohrob1@gmail.com>
2021-10-28 21:15:58 -0400

    friends page list

--- frontend/components/sidebar-pages/friend-page.jsx
+++ frontend/components/sidebar-pages/friend-page.jsx
@@ -3,13 +3,16 @@ import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { formatFullName } from '../../../../util/format_name';
 import FriendsIndex from '../friends_index';
-
+import { getFriends } from '../../reducers/selectors/friend_selector';
+import { getUser } from '../../actions/user_actions';
 const mSTP = state => ({
-
+  user: state.entities.users[state.session.id],
+  friendsriends: getFriends(state.session.id, state.entities.friends),
+  users: state.entities.users
 })
 
 const mDTP = dispatch => ({
-
+  getUser: id => dispatch(getUser(id))
 })
 
 
@@ -46,7 +49,19 @@ class FriendsPage extends React.Component {
         friendId = friend.user_id2;
       }
       //debugger;
-      return <FriendsIndex handleButtonClick={this.props.handleButtonClick} friendId={friendId} key={idx}/>
+      let user = this.props.user[friendId];
+      return (//<FriendsIndex friendId={friendId} key={idx}/>
+        <div className='user-card-container' key={idx}>
+          <div className='user-card-header'>
+        <Link to={`/users/${friendId}`}><img src={user.profile_photo} className='user-img'></img>
+          
+        </Link>
+          </div>
+          <div className='user-card-body'>
+            <Link to={`/users/${friendId}`}>{formatFullName(user.fname,user.lname)}</Link>
+          </div>
+        </div>
+        )
     })
    //debugger;
     return (
